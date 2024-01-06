f873c44db0cfb54b3ce85e56fe53870e77f161cb
Stephen Aylward <stephen.aylward@kitware.com>
2024-01-06 09:22:50 -0500

    feat(tube_viewer): Enable selection of multiple tubes and tube stats

--- src/TubeViewer.py
+++ src/TubeViewer.py
@@ -7,7 +7,7 @@ def main():
     args = sys.argv[1:]
     if len(args) == 1:
         grp = read_group(args[0])
-        viewer2 = tube_viewer(tubes=vgrp_base)
+        viewer2 = tube_viewer(tubes=grp)
         viewer2.render_tubes_as_surfaces(param="Radius")
     else:
         print("USAGE: tubeviewer <filename.tre>")
